c46dc63a7af06feb8fb5cc3a8246756179c29d8e
ElvineJoy <elvinkuiyungest@gmail.com>
2024-07-09 13:55:48 +0300

    First test passing

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = []
+    for number in num_list:
+        if (number % 2 == 0):
+            evens.append(number)
+    
+    print(evens)
+
+return_evens([0, 1, 3, 5, 7, 8, 9])      
 
 def make_exclamation(sentence_list):
     pass
